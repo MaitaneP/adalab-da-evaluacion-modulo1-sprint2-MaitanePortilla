611750a317fb8049a7d59c4a98ce23d9886bb28b
MaitaneP <portillamaitane@gmail.com>
2023-03-22 17:55:24 +0100

    repo reorganizado, ruta python modificada y enunciado sql

---
+++ SQL/evaluacion-sql-2.sql
@@ -0,0 +1,40 @@
+#  EJERCICIOS 2ª EVALUACIÓN SQL. MÓDULO 1 - SPRINT 2
+-- --------------------------------------------------
+/* Para esta evaluación usaremos la BBDD de northwind con la que ya estamos familiarizadas de los ejercicios de pair programming. 
+En esta evaluación tendréis que contestar a las siguientes preguntas: */
+
+USE northwind;
+
+/* Selecciona todos los campos de los productos, que pertenezcan a los proveedores con códigos: 1, 3, 7, 8 y 9, que tengan stock en 
+el almacén, y al mismo tiempo que sus precios unitarios estén entre 50 y 100. Por último, ordena los resultados por código de proveedor 
+de forma ascendente.*/
+
+
+
+
+/* Devuelve el nombre y apellidos y el id de los empleados con códigos entre el 3 y el 6, además que hayan vendido a clientes que tengan 
+códigos que comiencen con las letras de la A hasta la G. Por último, en esta búsqueda queremos filtrar solo por aquellos envíos que la 
+fecha de pedido este comprendida entre el 22 y el 31 de Diciembre de cualquier año.*/
+
+
+
+/* Calcula el precio de venta de cada pedido una vez aplicado el descuento. Muestra el id del la orden, el id del producto, el nombre del 
+producto, el precio unitario, la cantidad, el descuento y el precio de venta después de haber aplicado el descuento.*/
+
+
+
+/* Usando una subconsulta, muestra los productos cuyos precios estén por encima del precio medio total de los productos de la BBDD.*/
+
+
+
+/* ¿Qué productos ha vendido cada empleado y cuál es la cantidad vendida de cada uno de ellos?*/
+
+
+
+/* Basándonos en la query anterior, ¿qué empleado es el que vende más productos? Soluciona este ejercicio con una subquery*/
+
+
+
+/* BONUS ¿Podríais solucionar este mismo ejercicio con una CTE?*/
+
+
